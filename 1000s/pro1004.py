48dfb33b8269449e52c10531f37e9bfa42a6026a
June Song <June Song>
2016-10-03 22:42:22 +0900

    solving this problem

---
+++ 1000s/pro1004.py
@@ -0,0 +1,10 @@
+# get number of test cases
+numb = int(raw_input().strip())
+
+# get queries from the cases
+for n in range(numb):
+    # get points
+    points = int(raw_input().strip().split(' '))
+    # get number of planets
+    planets = int(raw_input().strip())
+    # do print
